a1521de693448c6363c13734ee8bc2d263bb9e7e
Niels Joubert <njoubert@gmail.com>
2010-09-18 02:54:04 -0700

    we now map an mp3 file to memory

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 
-TARGET := shazam
+TARGET := shablam
 
 INCLUDE := -I/opt/local/include
 LIB := -lmad
 LIBDIRS := -L/opt/local/lib
 
-main.o: main.cpp
-	g++ $(INCLUDE) -o main.o -c main.cpp
-
 all: main.o
 	g++ $(LIBDIRS) $(LIB) -o $(TARGET) main.o
+
+main.o: main.cpp
+	g++ $(INCLUDE) -o main.o -c main.cpp
 
 clean:
 	rm -rf *.o $(TARGET)
--- main.cpp
+++ main.cpp
@@ -1,5 +1,55 @@
 #include <mad.h>
 
-int main(int argc, char **argv) {
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
+
+int optDecode(int argc, char **argv) {
+	if (argc != 3) {
+		printf("You need to specify a .mp3 input file.\n");
+		return 1;
+	}
+	char* inputFile = argv[2];
+	void *fdm;
+	
+	FILE* fi = fopen(inputFile, "r");
+	struct stat stat;
+	if (fi == NULL || fstat(fileno(fi), &stat) == -1 || stat.st_size == 0) {
+		printf("%s does not exist or is empty.\n", inputFile);
+		return 2;
+	}
+	
+	printf("Processing %s... [%ld bytes]\n", inputFile, (long) stat.st_size);
+	
+	fdm = mmap(0, stat.st_size, PROT_READ, MAP_SHARED, fileno(fi), 0);
+	if (fdm == MAP_FAILED) {
+		printf("Could not map file to memory.\n");
+		return 3;
+	}
+	
+	
+	if (munmap(fdm, stat.st_size) == -1) {
+		printf("Could not unmap file's memory.\n");
+		return 4;
+	}
+	return 0;
 }
+
+
+int main(int argc, char **argv) {
+	
+	if (argc < 2) {
+		printf("Need to specify a command switch!\n");
+		return 1;
+	}
+	if (strcmp(argv[1],"-d") == 0) {
+		return optDecode(argc, argv);
+	}
+	printf("Did not match any of the options.\n");
+	return 1;
+}
+
+
